d6e52f643b2fbe243aeb253e8c0544087dd41770
brittany duncan <duncandakota08@gmail.com>
2023-11-13 13:57:44 -0500

    complete tests

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+Math.round(Math.random() * 10) + 1;
+let random = num1 * num2
+
+let num3 = 2
+let num4 = 8
+let mod = num4 / num3
+
+let max = Math.max(20)
